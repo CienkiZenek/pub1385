0bb8b9f5a92f855bdd11ce2c4207c30cfadb7f9b
CienkiZenek <w.operacz@poczta.onet.pl>
2021-09-29 19:00:46 +0000

    pom-29-09-b

--- resources/views/dodatki/karuzela.blade.php
+++ resources/views/dodatki/karuzela.blade.php
@@ -23,8 +23,9 @@
                 {{--<img src="{{$karuzela->Urlobrazek1}}" class="d-block w-100" alt="...">--}}
 
                 <div class="carousel-caption d-none d-md-block">
-                    <a href="{{route('zagadnienieCale',$karuzela->slug )}}" class="text-white text-decoration-none"><h5 class="tlo-karuzela p-1 rounded-3">{{$karuzela->zajawka_tytul}}</h5>
-                    <p class="tlo-karuzela p-1  rounded-3">{{$karuzela->zajawka}}</p></a>
+                    <a href="{{route('zagadnienieCale',$karuzela->slug )}}" class="text-white text-decoration-none">
+                        {{--<h5 class="tlo-karuzela p-1 rounded-3">{{$karuzela->zajawka_tytul}}</h5>--}}
+                    <p class="tlo-karuzela p-1  rounded-3">{{Str::limit($karuzela->zajawka, 170)}}</p></a>
                 </div>
             </div>
 
